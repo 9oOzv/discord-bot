3c0b7446dbfc590b3d8085696b840dcf77333610
Miika Piiroinen <miika.piiroinen@gmail.com>
2021-12-18 06:43:25 +0200

    Initial code

---
+++ index.js
@@ -0,0 +1,44 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require('discord.js');
+
+const config = require('config.json');
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+
+const rest = new REST({ version: '9' }).setToken(config.token);
+
+async function registerCommands() {
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(
+      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      { body: commands },
+    );
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function start() {
+  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
+  client.login('token');
+}
+
+
+registerCommands()
+  .then(start)
